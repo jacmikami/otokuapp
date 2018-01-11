5b664854fb09c8e0abf13953e37cc7182a748b78
jacmikami <t-mikami@jacopen.co.jp>
2018-01-12 00:08:29 +0900

    Initial Commit

---
+++ otokuapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  otokuapp
+//
+//  Created by 三上智幸 on 2018/01/12.
+//  Copyright © 2018年 tomoyuki mikami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
